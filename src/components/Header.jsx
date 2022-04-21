b7757caba1eb1f08595ba4cac280431ced003052
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-21 12:13:52 +0200

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,72 @@
-import { Container } from '@chakra-ui/react'
+import {
+  Flex,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Box,
+  Center,
+  Heading,
+  Spacer,
+  Container,
+} from "@chakra-ui/react";
+
+import { Link } from "react-router-dom";
+
+import { SearchIcon } from "@chakra-ui/icons";
 
 function Header() {
-  return ( 
-    <Container>
-      <Heading as='h1' size='4xl'>
-      Hey Socialmedia
-      </Heading>
+  return (
+    <Container maxW="100%">
+      <Flex>
+        <Center gap={10}>
+          <Link to="/">
+            <Heading as="h1" size="2xl">
+              Hey Socialmedia
+            </Heading>
+          </Link>
+
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input type="tel" placeholder="Search for posts" />
+          </InputGroup>
+        </Center>
+
+        <Spacer />
+        <Center>
+          <Flex gap={6}>
+            <Link to="/login">
+              <Box
+                as="button"
+                borderRadius="md"
+                bg="#00A3C4"
+                color="white"
+                px={4}
+                h={8}
+              >
+                Log in
+              </Box>
+            </Link>
+
+            <Link to="/signup">
+              <Box
+                as="button"
+                borderRadius="md"
+                bg="#00A3C4"
+                color="white"
+                px={4}
+                h={8}
+              >
+                Sign up
+              </Box>
+            </Link>
+          </Flex>
+        </Center>
+      </Flex>
     </Container>
-   );
+  );
 }
 
 export default Header;
