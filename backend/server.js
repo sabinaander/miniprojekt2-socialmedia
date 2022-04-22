e904e36820c53fb859a33bdc7a42adb09ef5fa37
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-22 16:36:14 +0200

    added cookies

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const connectDB = require('./config/db')
-
+const cookieSession = require('cookie-session')
 connectDB()
 
 const port = 5000
@@ -12,11 +12,21 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
+app.use(cookieSession({
+  // fix secret with env later...
+  secret: '1337',
+  httpOnly: true,
+  maxAge: 24 * 60 * 60 * 1000,
+  sameSite: 'strict',
+  secure: false,
+}))
 
 app.use('/api/blogPosts', require('./routes/blogPostRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
-
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`.magenta)
