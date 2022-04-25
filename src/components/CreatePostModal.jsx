6ada7a4d6ce703aef07729769f0740ef45ce1bf7
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-25 15:16:57 +0200

    use redux for seeing the createpostmodal component

--- src/components/CreatePostModal.jsx
+++ src/components/CreatePostModal.jsx
@@ -12,18 +12,30 @@ import {
 } from "@chakra-ui/react";
 
 import { useDisclosure } from "@chakra-ui/react";
+import {  useState } from "react";
+import { useStore } from "react-redux";
 import PostForm from "../components/PostForm";
+import loginauthreducer from "../features/login-auth/reducers/loginauthreducer";
 
 function CreatePostModal() {
-  const currentUser = JSON.parse(localStorage.getItem("user"));
+  const store = useStore(loginauthreducer);
+  const state = store.getState();
+  const [isLoggedIn, setIsLoggedIn] = useState(state.auth.isLoggedIn);
+  const [user, setUser] = useState(state.auth.user);
+
+  store.subscribe(() => {
+    setIsLoggedIn(store.getState().auth.isLoggedIn);
+    setUser(store.getState().auth.user);
+  });
   
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
+    user &&
     <Center padding="0.5rem" gap={{base:"0.5rem", lg: "2rem"}} w="100%" mb={3}>
       <Avatar
         size="xl"
-        name={currentUser?.username}
-        src={currentUser?.avatar}
+        name={user.username}
+        src={user.avatar}
       />
       <Button
         onClick={onOpen}
