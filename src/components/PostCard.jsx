bfc2c4292366b91d84578dc8a1411ed2491caab7
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-04-25 08:04:06 +0200

    removed mockpost. get data from props

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,20 @@
-import { Box, Image } from "@chakra-ui/react";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Box, Image } from '@chakra-ui/react'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
-function PostCard() {
-  const post = {
-    imageUrl:
-      "https://cdn.gameloot.se/5052-thickbox_default/marvel-avengers-staty---thanos-16.jpg",
-    imageAlt: "nice pool",
-    author: "username",
-    date: "date",
-    title: "Omg, this day was so nice!",
-    description: "me and mama traveled to see Thanos <3",
-    likes: "53",
-  };
-
+function PostCard({ post }) {
   return (
-    <Box maxW="sm" bg="white" borderWidth="1px" borderRadius="lg" overflow="hidden">
+    <Box
+      key={post._id}
+      maxW="sm"
+      bg="white"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+    >
       <Image src={post.imageUrl} alt={post.imageAlt} />
 
       <Box p="6">
-        <Box  display="flex" alignItems="baseline">
+        <Box display="flex" alignItems="baseline">
           <Box
             color="gray.500"
             fontWeight="semibold"
@@ -27,7 +23,7 @@ function PostCard() {
             textTransform="uppercase"
             ml="2"
           >
-            {post.author} &bull; {post.date}
+            {post.author} &bull; {post.createdAt}
           </Box>
         </Box>
 
@@ -47,7 +43,7 @@ function PostCard() {
         </Box>
 
         <Box display="flex" mt="2" alignItems="center">
-        <FavoriteIcon fontSize="medium" color="action" />
+          <FavoriteIcon fontSize="medium" color="action" />
           Like
           <Box as="span" ml="2" fontSize="sm">
             {post.likes} likes
@@ -66,7 +62,7 @@ function PostCard() {
         </Box>
       </Box>
     </Box>
-  );
+  )
 }
 
-export default PostCard;
+export default PostCard
