b8ce0eddf737c20c3a482133bc15010d759cef14
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-04-25 08:00:22 +0200

    route added for single post

--- backend/routes/blogPostRoutes.js
+++ backend/routes/blogPostRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {getBlogPosts,editBlogPosts, addBlogPosts, deleteBlogPosts} = require('../controllers/blogPostController')
+const {
+  getBlogPosts,
+  getBlogPost,
+  editBlogPosts,
+  addBlogPosts,
+  deleteBlogPosts,
+} = require('../controllers/blogPostController')
 
 router.route('/').get(getBlogPosts).post(addBlogPosts)
-router.route('/:id').put(editBlogPosts).delete(deleteBlogPosts)
-
+router.route('/:id').get(getBlogPost).put(editBlogPosts).delete(deleteBlogPosts)
 
 module.exports = router
