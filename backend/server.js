b68f8f445634cf768258355207e9ea929c5ecd84
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-04-25 07:57:34 +0200

    cors and colors Fixes #5

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const colors = require('colors')
+require('colors')
 const cors = require('cors')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const connectDB = require('./config/db')
 
 connectDB()
@@ -12,14 +12,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.use('/api/blogPosts', require('./routes/blogPostRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-
 app.listen(port, () => {
   console.log(`server running on port ${port}`.magenta)
 })
-
-
-
