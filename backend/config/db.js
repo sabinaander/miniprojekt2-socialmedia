d76306ae41c5331970826463daaa1e45d74e39cf
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-04-25 07:58:17 +0200

    bugfix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    
-    const conn = await mongoose.connect('mongodb://localhost:27017/socialmedia');
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
+    const conn = await mongoose.connect('mongodb://localhost:27017/socialmedia')
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
+  } catch (err) {
     if (err) console.log(err)
+
     process.exit(1)
   }
 }
