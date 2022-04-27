e873bb19e24e7f0bb0cb90618a4df4acf39c381f
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-27 17:11:51 +0200

    added cookie security to delete user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -128,7 +128,23 @@ const getUser = asyncHandler(async (req, res) => {
 // delete user by username
 const deleteUser = asyncHandler(async (req, res) => {
 
+  // checks to see if cookie session id is existing
+  if(!req.session.id){
+    res.status(401);
+    res.send({ message: 'Unauthorized save attempt.' });
+    return;
+  }
+
   const username = req.params.username
+
+  // todo: maybe allow admins.   
+  //(now checks so the cookie name and localstorage name is same)
+  if(req.session.username !== username){
+    res.status(403);
+    res.send({ message: 'Not allowed to update other users.' });
+    return;
+  }
+  
   const user = await User.findOne({ username: username }).exec()
 
 
