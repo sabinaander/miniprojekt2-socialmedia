eae16fcc31bd49221cfeabc6ae905470cc45c51a
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-27 17:39:33 +0200

    fixed profile header

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,5 +1,4 @@
 import {
-  Container,
   Box,
   Button,
   Avatar,
@@ -11,20 +10,25 @@ import {
   FormControl,
   Input,
   Center,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Modal,
 } from "@chakra-ui/react";
-import { CloseIcon, EditIcon } from "@chakra-ui/icons";
+import { EditIcon } from "@chakra-ui/icons";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { update } from "../features/login-auth/loginauth";
 
 function ProfileHeader(props) {
-  const [EditModeBackGroundImage, setEditModeBackGroundImage] = useState(false);
-  const [editModeAvatar, setEditModeAvatar] = useState(false);
-
   const toast = useToast();
   const [errorMessage, setErrorMessage] = useState("");
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const backgroundDisclosure = useDisclosure();
+  const avatarDisclosure = useDisclosure();
 
   const {
     register,
@@ -50,9 +54,10 @@ function ProfileHeader(props) {
         duration: 4000,
         isClosable: true,
       });
-      setEditModeAvatar(false);
-      setEditModeBackGroundImage(false);
-      props.loadUser(props.profileUser.username)
+      avatarDisclosure.onClose()
+      backgroundDisclosure.onClose()
+
+      props.loadUser(props.profileUser.username);
     } catch (e) {
       console.log(e);
       setErrorMessage(e.response.data.message);
@@ -63,16 +68,17 @@ function ProfileHeader(props) {
   return (
     props.profileUser && (
       <Box maxW="100%">
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form id="bioForm" onSubmit={handleSubmit(onSubmit)}>
           <Box height="30vh" position="relative">
             <Box
               position="absolute"
               width="100%"
-              backgroundColor="black"
+              backgroundColor="gray"
               height="100%"
               left="0"
               role="group"
               backgroundImage={props.profileUser.backgroundimage}
+              backgroundSize="cover"
             >
               {!!props.isLoggedIn &&
                 props.profileUser.username === props.authUser.username && (
@@ -85,39 +91,48 @@ function ProfileHeader(props) {
                     ml={5}
                     w={6}
                     h={6}
-                    onClick={() => setEditModeBackGroundImage(true)}
+                    onClick={backgroundDisclosure.onOpen}
                   ></EditIcon>
                 )}
             </Box>
 
-            {EditModeBackGroundImage && (
-              <Box>
-                <Center gap="1rem" mb={2}>
-                  <FormControl>
-                    <Input
-                      {...register("backgroundimage", {
-                        required: true,
-                        value: props.profileUser.backgroundimage,
-                      })}
-                    />
-                  </FormControl>
+              <Modal
+                closeOnOverlayClick={false}
+                isOpen={backgroundDisclosure.isOpen}
+                onClose={backgroundDisclosure.onClose}
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Edit background image</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody pb={6}>
+                    <Center gap="1rem" mb={2}>
+                      <FormControl>
+                        <Input
+                          {...register("backgroundimage", {
+                            required: true,
+                            value: props.profileUser.backgroundimage,
+                          })}
+                        />
+                      </FormControl>
+                    </Center>
 
-                  <CloseIcon
-                    border="solid 2px"
-                    borderRadius={4}
-                    p={1}
-                    w={6}
-                    h={6}
-                    cursor="pointer"
-                    onClick={() => setEditModeBackGroundImage(false)}
-                  />
-                </Center>
+                    <Button
+                      form="bioForm"
+                      type="submit"
+                      colorScheme="purple"
+                      isFullWidth
+                    >
+                      Save edit
+                    </Button>
+                  </ModalBody>
 
-                <Button type="submit" colorScheme="purple" isFullWidth>
-                  Save edit
-                </Button>
-              </Box>
-            )}
+                  <ModalFooter>
+                    <Button onClick={backgroundDisclosure.onClose}>Cancel</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+           
             <Box position="relative" w="min-content" padding={3}>
               <Box position="relative" role="group" w="min-content">
                 <Avatar
@@ -137,42 +152,51 @@ function ProfileHeader(props) {
                       ml={5}
                       w={6}
                       h={6}
-                      onClick={() => setEditModeAvatar(true)}
+                      onClick={avatarDisclosure.onOpen}
                     ></EditIcon>
                   )}
               </Box>
-              <Heading as="h1" size="2xl" color="white" >
+              <Heading as="h1" size="2xl" color="white">
                 {props.profileUser.username}
               </Heading>
             </Box>
-            {editModeAvatar && (
-              <Box>
-                <Center gap="1rem" mb={2}>
-                  <FormControl>
-                    <Input
-                      {...register("avatar", {
-                        required: true,
-                        value: props.profileUser.avatar,
-                      })}
-                    />
-                  </FormControl>
+              <Modal
+                closeOnOverlayClick={false}
+                isOpen={avatarDisclosure.isOpen}
+                onClose={avatarDisclosure.onClose}
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Edit avatar picture</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody pb={6}>
+                    <Center gap="1rem" mb={2}>
+                      <FormControl>
+                        <Input
+                          {...register("avatar", {
+                            required: true,
+                            value: props.profileUser.avatar,
+                          })}
+                        />
+                      </FormControl>
+                    </Center>
 
-                  <CloseIcon
-                    border="solid 2px"
-                    borderRadius={4}
-                    p={1}
-                    w={6}
-                    h={6}
-                    cursor="pointer"
-                    onClick={() => setEditModeAvatar(false)}
-                  />
-                </Center>
+                    <Button
+                      form="bioForm"
+                      type="submit"
+                      colorScheme="purple"
+                      isFullWidth
+                    >
+                      Save edit
+                    </Button>
+                  </ModalBody>
 
-                <Button type="submit" colorScheme="purple" isFullWidth>
-                  Save edit
-                </Button>
-              </Box>
-            )}
+                  <ModalFooter>
+                    <Button onClick={avatarDisclosure.onClose}>Cancel</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            
             <Flex>
               <Spacer />
             </Flex>
