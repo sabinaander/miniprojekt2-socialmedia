0fdb2f82132288dcbee2a4f34ad45c10c3a9bc2e
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-04-28 09:29:10 +0200

    small fix to conditional

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,38 +1,32 @@
-import { Outlet } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
-import "../App.css"
-import Header from "./Header";
-import SideBar from "./SideBar";
-import { useStore } from "react-redux";
-import loginauthreducer from "../features/login-auth/reducers/loginauthreducer";
-import { useState } from "react";
+import { Outlet } from 'react-router-dom'
+import { Flex } from '@chakra-ui/react'
+import '../App.css'
+import Header from './Header'
+import SideBar from './SideBar'
+import { useStore } from 'react-redux'
+import loginauthreducer from '../features/login-auth/reducers/loginauthreducer'
+import { useState } from 'react'
 
 function Layout() {
-  const store = useStore(loginauthreducer);
-  const state = store.getState();
-  const [isLoggedIn, setIsLoggedIn] = useState(state.auth.isLoggedIn);
-  const [user, setUser] = useState(state.auth.user);
+  const store = useStore(loginauthreducer)
+  const state = store.getState()
+  const [isLoggedIn, setIsLoggedIn] = useState(state.auth.isLoggedIn)
+  const [user, setUser] = useState(state.auth.user)
 
   store.subscribe(() => {
-    setIsLoggedIn(store.getState().auth.isLoggedIn);
-    setUser(store.getState().auth.user);
-  });
+    setIsLoggedIn(store.getState().auth.isLoggedIn)
+    setUser(store.getState().auth.user)
+  })
   return (
     <div className="App">
-      <header>
-        <Header />
-        <Flex >
-          {/* Only show this if logged in */}
-          {isLoggedIn ? (
-        <SideBar />
-        ):(
-          <></>
-        )}
-        <Outlet/>
-        </Flex>
-      </header>
+      <Header />
+      <Flex>
+        {/* Only show this if logged in */}
+        {isLoggedIn && <SideBar />}
+        <Outlet />
+      </Flex>
     </div>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
