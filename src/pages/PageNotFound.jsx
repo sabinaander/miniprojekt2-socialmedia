687f298247920c6e5705c56663b251447d376003
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-29 09:54:22 +0200

    Added pagenotfound component

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import { Box, Center, Container, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <Container>
+      <Center mt="10rem">
+        <Heading>Oops! Page not found!</Heading>
+      </Center>
+    </Container>
+  );
+}
+
+export default PageNotFound;
